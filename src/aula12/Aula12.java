7e3edaab805f81292f10d7628c16a8904185750a
Jonas Valereo <jvalereo2016@gmail.com>
2021-04-13 00:05:52 -0300

    Initial commit

---
+++ src/aula12/Aula12.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12;
+
+/**
+ *
+ * @author JONAS
+ */
+public class Aula12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Treinamento - Aula12, inserir dados, tipo double, int */
+        //Jonas valereo - Técnico em Informática
+        
+        //Declarando a variavel tipo double, int
+        
+        double a = 20.5;
+        double b = 30.0;
+        int resultado;
+        
+       
+        /*Declarando a variavel resultado, Casting conversão 
+        explicita de tipo primitivo para outro*/
+        
+        resultado = (int) a + (int) b;
+        
+        
+        //Imprimir, saida de dados, Console System
+        
+        System.out.println("Resultado = " + resultado);
+        
+
+       //Saida da aplicação e descarregar a mamoria
+        
+        System.exit(0);
+        
+        //Fim do programa
+        
+    }
+    
+}
